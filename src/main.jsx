546b2ae546d316f7a03dfada74c366a0dcd0242d
vijilaa <vijilajames221@gmail.com>
2026-01-21 16:35:46 +0530

    Fix bootstrap dependency

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import'bootstrap/dist/css/bootstrap.min.css'
+
 
 import Router from './Router.jsx';
 
